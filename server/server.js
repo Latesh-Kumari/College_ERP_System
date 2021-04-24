8e0425fb11999a45c9a5a401e523aa8501c0042f
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2021-04-24 16:11:31 +0530

    added socket in server for enable messaging

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const http = require('http')
+const socket =  require('socket.io')
 const mongoose = require('mongoose')
 const passport = require('passport')
-const cookieParser = require('cookie-parser');
-const cors = require('cors')
-const morgan = require('morgan')
 
 const dotenv = require('dotenv');
 dotenv.config()
 
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
+const morgan = require('morgan')
+
+
 //MIDDILWARES
 const app = express();
 let server = http.createServer(app);
-
+let io = socket(server);
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 app.use(cookieParser())
@@ -26,6 +29,8 @@ app.use(function (req, res, next) {
     next();
 });
 
+// Import for Routes
+
 //Passport Middleware
 app.use(passport.initialize());
 
@@ -34,6 +39,23 @@ app.use(passport.initialize());
 
 app.use(morgan('dev'))
 
+io.on('connection', (socket) => {
+    socket.on('join room', ({room1, room2}) => {
+        socket.join(room1)
+        socket.join(room2)
+    })
+    socket.on("private message", (message) => {
+        io.to(message.room).emit('new Message', {
+            message: message.message,
+            sender: message.sender
+        });
+    })
+    socket.on('disconnect', function () {
+        console.log('Socket disconnected');
+    })
+})
+
+// Routes
 
 //Catching 404 Error
 app.use((req, res, next) => {
