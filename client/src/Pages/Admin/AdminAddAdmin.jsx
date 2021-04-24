ee985abcdf82d6bf08d37f2a5d9b140b75f87521
Asus <latesh.kumari_cs19@gla.ac.in>
2021-04-24 20:25:43 +0530

    import react.

---
+++ client/src/Pages/Admin/AdminAddAdmin.jsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { useHistory } from 'react-router-dom'
+// import classnames from 'classnames'
+// import { adminAddAdmin } from '../../redux/action/adminAction'
+// import AdminHomeHelper from '../../Components/AdminHomeHelper'
