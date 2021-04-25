d9a164d9399329241682c60f8a553a1c4661fc7a
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2021-04-25 13:18:04 +0530

    add : API for student to send message to receiver name.

--- server/controller/studentController.js
+++ server/controller/studentController.js
@@ -257,4 +257,53 @@ module.exports = {
 
         }
     },
+    differentChats: async (req, res, next) => {
+        try {
+            const { receiverName } = req.params
+            const newChatsTemp = await Message.find({ senderName: receiverName })
+            // if (newChatsTemp.length === 0) {
+            //    return res.status(404).json({ result: "No any new Chat" })
+            // }
+            var filteredObjTemp = newChatsTemp.map(obj => {
+                let filteredObjTemp = {
+                    senderName: obj.senderName,
+                    receiverName: obj.receiverName,
+                    senderRegistrationNumber: obj.senderRegistrationNumber,
+                    receiverRegistrationNumber: obj.receiverRegistrationNumber,
+                    receiverId: obj.receiverId
+                }
+                return filteredObjTemp
+            })
+            let filteredListTemp = [...new Set(filteredObjTemp.map(JSON.stringify))].map(JSON.parse)
+
+            // const { receiverName } = req.params
+            const newChats = await Message.find({ receiverName })
+            // if (newChats.length === 0) {
+            //    return res.status(404).json({ result: "No any new Chat" })
+            // }
+            var filteredObj = newChats.map(obj => {
+                let filteredObj = {
+                    senderName: obj.senderName,
+                    receiverName: obj.receiverName,
+                    senderRegistrationNumber: obj.senderRegistrationNumber,
+                    receiverRegistrationNumber: obj.receiverRegistrationNumber,
+                    receiverId: obj.receiverId
+                }
+                return filteredObj
+            })
+            let filteredListPro = [...new Set(filteredObj.map(JSON.stringify))].map(JSON.parse)
+            for (var i = 0; i < filteredListPro.length; i++) {
+                for (var j = 0; j < filteredListTemp.length; j++) {
+                    if (filteredListPro[i].senderName === filteredListTemp[j].receiverName) {
+                        filteredListPro.splice(i, 1)
+
+                    }
+                }
+            }
+            res.status(200).json({ result: filteredListPro })
+        }
+        catch (err) {
+            console.log("Error in getting different chats", err.message)
+        }
+    },
 }
